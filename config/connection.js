cde86bec636b85e909c2fac38ce198ffb67fc497
warlording <warlording@gmail.com>
2018-03-02 22:35:13 -0600

    final

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'daves2045',
+		database: 'burgers_db'
+	})
+};
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Stinky Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Trap Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Great Meat Wall Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gary Busey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Triple Decker Cheesey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Boi Burger', true);
